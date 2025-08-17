446fac716c64ebd065fbc5dc083dab2647733954
mckwk <147665058+mckwk@users.noreply.github.com>
2025-08-17 13:31:58 +0200

    android local changes

---
+++ frontend/android/app/src/main/java/com/mckwk/scamalyzer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mckwk.scamalyzer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mckwk.scamalyzer',
+  appName: 'Scamalyzer',
+  webDir: 'build'
+};
+
+export default config;
